e5987492978327e5abc0d91e294afdb41fd0a355
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2022-08-29 19:41:33 -0700

    Initial Commit

---
+++ blackjack-21/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  blackjack-21
+//
+//  Created by Lance Townsend on 8/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
